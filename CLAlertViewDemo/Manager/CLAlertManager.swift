6d3a35ab8a3775efdd9e7b221a2b2b79fd972a3b
cleven <543069316@qq.com>
2021-05-14 17:55:16 +0800

    实现多层alert弹出控制

--- CLAlertViewDemo/Manager/CLAlertManager.swift
+++ CLAlertViewDemo/Manager/CLAlertManager.swift
@@ -10,34 +10,34 @@ import UIKit
 
 public let cl_screenWidht = UIScreen.main.bounds.width
 public let cl_screenHeight = UIScreen.main.bounds.height
-class CLAlertManager: NSObject {
-    
+class GKAlertManager: NSObject {
+    private struct GKAlertViewCache {
+        var view: UIView?
+        var index: Int = 0
+    }
     enum AlertPosition {
         case center
         case bottom
     }
     
-    private static var vc:UIViewController?
-    private static var containerView:UIButton?
-    private static var contentView: UIView?
+    private static var vc: UIViewController?
+    private static var containerView: UIButton?
     private static var currentPosition: AlertPosition = .center
+    private static var viewCache: [GKAlertViewCache] = []
     
-    public static func show(view: UIView, alertPostion: AlertPosition = .center, didCoverDismiss: Bool = false){
-        contentView = view
+    public static func show(view: UIView,
+                            alertPostion: AlertPosition = .center,
+                            didCoverDismiss: Bool = false){
+        let index = viewCache.isEmpty ? 0 : viewCache.count
+        viewCache.append(GKAlertViewCache(view: view, index: index))
         currentPosition = alertPostion
-        containerView = UIButton(frame: CGRect(x: 0,
-                                               y: 0,
-                                               width: cl_screenWidht,
-                                               height: cl_screenHeight))
-        if didCoverDismiss {
-            containerView?.addTarget(self,
-                                     action: #selector(tapView),
-                                     for: .touchUpInside)
+        if vc == nil {
+            containerView = UIButton(frame: CGRect(x: 0, y: 0, width: cl_screenWidht, height: cl_screenHeight))
+            containerView?.backgroundColor = UIColor(red: 0.0/255, green: 0.0/255, blue: 0.0/255, alpha: 0.0)
         }
-        containerView?.backgroundColor = UIColor(red: 0.0/255,
-                                                 green: 0.0/255,
-                                                 blue: 0.0/255,
-                                                 alpha: 0.0)
+        if didCoverDismiss {
+            containerView?.addTarget(self, action: #selector(tapView), for: .touchUpInside)
+        }
         containerView?.addSubview(view)
         view.alpha = 0
         if alertPostion == .center {
@@ -46,75 +46,104 @@ class CLAlertManager: NSObject {
             }
         }else{
             view.snp.makeConstraints { (make) in
-                make.top.equalTo(containerView!.snp.bottom)
+                make.bottom.equalToSuperview()
                 make.leading.trailing.equalToSuperview()
             }
         }
-        vc = UIViewController()
-        vc?.view.backgroundColor = UIColor.clear
-        vc?.view.addSubview(containerView!)
-        vc?.modalPresentationStyle = .custom
-        UIViewController.cl_topViewController()?.present(vc!, animated: false) {
-            if alertPostion == .center {
-                showCenterView(view: view)
-            }else{
-                showBottomView(view: view)
+        if vc == nil {
+            vc = UIViewController()
+            vc?.view.backgroundColor = UIColor.clear
+            vc?.view.addSubview(containerView!)
+            vc?.modalPresentationStyle = .custom
+            UIViewController.cl_topViewController()?.present(vc!, animated: false) {
+                showAlertPostion(alertPostion: alertPostion, view: view)
             }
+        } else {
+            showAlertPostion(alertPostion: alertPostion, view: view)
         }
-        // 监听键盘弹出通知
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name:UIResponder.keyboardWillShowNotification,object: nil)
-        // 监听键盘隐藏通知
-        NotificationCenter.default.addObserver(self,selector: #selector(keyboardWillHide(notification:)),name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private static func showAlertPostion(alertPostion: AlertPosition, view: UIView) {
+        containerView?.layoutIfNeeded()
+        if alertPostion == .center {
+            showCenterView(view: view)
+        }else{
+            view.snp.updateConstraints { (make) in
+                make.bottom.equalToSuperview().offset(view.height)
+            }
+            containerView?.layoutIfNeeded()
+            showBottomView(view: view)
+        }
     }
     
     private static func showCenterView(view: UIView){
+        if viewCache.isNotEmpty {
+            viewCache.forEach({ $0.view?.alpha = 0 })
+        }
         UIView.animate(withDuration: 0.25, animations: {
             containerView?.backgroundColor = UIColor(red: 0.0/255,
                                                      green: 0.0/255,
                                                      blue: 0.0/255,
-                                                     alpha: 0.4)
+                                                     alpha: 0.5)
             view.alpha = 1.0
         })
     }
     
     private static func showBottomView(view: UIView){
+        if viewCache.isNotEmpty {
+            viewCache.forEach({ $0.view?.alpha = 0 })
+        }
         view.alpha = 1.0
-        view.frame.origin.y = cl_screenHeight
         view.snp.updateConstraints { (make) in
-            make.top.equalTo(containerView!.snp.bottom).offset(-view.frame.height)
+            make.bottom.equalToSuperview()
         }
         UIView.animate(withDuration: 0.25, animations: {
             containerView?.backgroundColor = UIColor(red: 0.0/255,
                                                      green: 0.0/255,
                                                      blue: 0.0/255,
-                                                     alpha: 0.4)
+                                                     alpha: 0.5)
+//            containerView?.layoutIfNeeded()
+            containerView?.superview?.layoutIfNeeded()
+        })
+    }
+
+    static func updateViewHeight() {
+        UIView.animate(withDuration: 0.25, animations: {
             containerView?.layoutIfNeeded()
         })
     }
     
-    public static func hiddenView(view: UIView = contentView ?? UIView()){
-        NotificationCenter.default.removeObserver(self)
+    static func hiddenView(all: Bool = true, completion: (() -> Void)? = nil){
         if currentPosition == .bottom {
-            view.snp.updateConstraints { (make) in
-                make.top.equalTo(containerView!.snp.bottom)
+            guard let lastView = viewCache.last?.view else { return }
+            lastView.snp.updateConstraints { (make) in
+                make.bottom.equalToSuperview().offset(lastView.frame.height)
             }
         }
         UIView.animate(withDuration: 0.25, animations: {
-            containerView?.backgroundColor = UIColor(red: 255.0/255,
-                                                     green: 255.0/255,
-                                                     blue: 255.0/255,
-                                                     alpha: 0.0)
-            if currentPosition == .center {
-                containerView?.alpha = 0.0
-            }else{
+            if all || viewCache.isEmpty {
+                containerView?.backgroundColor = UIColor(red: 255.0/255,
+                                                         green: 255.0/255,
+                                                         blue: 255.0/255,
+                                                         alpha: 0.0)
                 containerView?.layoutIfNeeded()
             }
-        }) { (_) in
-            vc?.dismiss(animated: false, completion: nil)
-        }
+            if currentPosition == .center {
+                viewCache.last?.view?.alpha = 0
+            }
+        }, completion: { (_) in
+            if all || viewCache.isEmpty {
+                vc?.dismiss(animated: false, completion: completion)
+                vc = nil
+            } else {
+                viewCache.removeLast()
+                viewCache.last?.view?.alpha = 1
+            }
+        })
     }
     
-    @objc private static func tapView(){
+    @objc
+    private static func tapView(){
         DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: UInt64(0.1))) {
             self.hiddenView()
         }
@@ -122,20 +151,20 @@ class CLAlertManager: NSObject {
     
     private static var originFrame:CGRect = .zero
     // 键盘显示
-    @objc private static func keyboardWillShow(notification: Notification) {
-        originFrame = contentView!.frame
-        let keyboardHeight = (notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? CGRect)?.height
-        let y = cl_screenHeight - (keyboardHeight ?? 304) - contentView!.frame.height - 20
-        UIView.animate(withDuration: 0.25) {
-            contentView?.frame.origin.y = y
-        }
-    }
-    // 键盘隐藏
-    @objc private static func keyboardWillHide(notification: Notification) {
-        UIView.animate(withDuration: 0.25) {
-            contentView?.frame = originFrame
-        }
-    }
+//    @objc private static func keyboardWillShow(notification: Notification) {
+//        originFrame = contentView!.frame
+//        let keyboardHeight = (notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? CGRect)?.height
+//        let y = cl_screenHeight - (keyboardHeight ?? 304) - contentView!.frame.height - 20
+//        UIView.animate(withDuration: 0.25) {
+//            contentView?.frame.origin.y = y
+//        }
+//    }
+//    // 键盘隐藏
+//    @objc private static func keyboardWillHide(notification: Notification) {
+//        UIView.animate(withDuration: 0.25) {
+//            contentView?.frame = originFrame
+//        }
+//    }
     
 }
 
